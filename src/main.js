683960fe9e6667ea18ee8d37b9ba7b424b1a9b61
yuzhipeng <rfstef19890116@126.com>
2019-07-19 14:21:09 +0800

    [+]多级路由练习

--- src/main.js
+++ src/main.js
@@ -10,6 +10,15 @@ import Register from "./components/Register"
 import About from "./components/about/About"
 import HelloWord from "./components/HelloWorld"
 // import Users from './components/Users.vue'
+//关于我们的二级路由
+import Contact from './components/about/Contact'
+import Delivery from './components/about/Delivery'
+import History from './components/about/History'
+import OrderingGuide from './components/about/OrderingGuide'
+//关于我们-联系我们三级路由
+import Phone from './components/about/contact/Phone'
+import Email from './components/about/contact/Email'
+
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
@@ -23,7 +32,19 @@ const router = new VueRouter({
     {path:"/login", name:"loginLink", component: Login},
     {path:"/menu", component: Menu},
     {path:"/register", component: Register},
-    {path:"/about", component: About},
+    {path:"/about", component: About, redirect: '/about/contact', children:
+        [
+          {path: '/about/contact', name: 'contactLink', component: Contact, redirect: '/about/contact/phone', children:
+                [
+                  {path: '/about/contact/phone', name: 'phoneLink', component: Phone},
+                  {path: '/about/contact/email', name: 'emailLink', component: Email}
+                ]
+          },
+          {path: '/about/delivery', name: 'deliveryLink', component: Delivery},
+          {path: '/about/history', name: 'historyLink', component: History},
+          {path: '/about/orderingGuide', name: 'orderingGuideLink', component: OrderingGuide}
+        ]
+    },
     {path:"/helloword", component: HelloWord},
     {path:"*", redirect:"/"}//设置如果前面的都没有匹配上，返回到根目录
   ],
